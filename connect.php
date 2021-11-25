ff73487c2072b1d57b737972876490b14fe0ad97
black-purple <82895430+black-purple@users.noreply.github.com>
2021-11-25 16:06:41 +0100

    Feat: Addidng signup feature

--- connect.php
+++ connect.php
@@ -1,21 +1,13 @@
 <?php
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $userName = $_POST['userName'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
 
-// connect to DB
-
-    $con = new mysqli('localhost', 'root', '', 'test');
-    if ($con->connect_error){
-        die('Connection failed : '.$con->connect_error);
-    } else {
-        $stmt = $con->prepare("insert into registration(firstName, lastName, userName, email, password) values(?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssss", $firstName, $lastName, $userName, $email, $password);
-        $stmt->execute();
-        echo "Registration success";
-        $stmt->close();
-        $con->close();
+    $con = new PDO("mysql:host=localhost,dbname=test","root","");
+if (isset($_POST['submit-btn'])){
+        $req = $con->prepare('insert into registration value (?,?,?,?,?)');
+        $req->bindValue(1,$_POST['firstName']);
+        $req->bindValue(2,$_POST['lastName']);
+        $req->bindValue(3,$_POST['userName']);
+        $req->bindValue(4,$_POST['email']);
+        $req->bindValue(5,$_POST['password']);
+        $req->execute();
     }
 ?>
